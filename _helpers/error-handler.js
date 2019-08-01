89de25f19c515643ecdb96560b1638b1720b4907
Yu, Qihui <qihui.yu@finastra.com>
2019-08-01 17:54:25 +0200

    FLE-100: user manangement backend api built with nodejs

---
+++ _helpers/error-handler.js
@@ -0,0 +1,32 @@
+module.exports = errorHandler;
+
+function errorHandler(err, req, res, next) {
+    if (typeof (err) === 'string') {
+        // custom application error
+        return res.status(400).json({ message: err });
+    }
+
+    if (err.name === 'ValidationError') {
+        // mongoose validation error
+        return res.status(400).json({ message: err.message });
+    }
+
+    if (err.name === 'UnauthorizedError') {
+        // jwt authentication error
+        return res.status(401).json({ message: 'Invalid Token' });
+    }
+
+    // default to 500 server error
+    return res.status(500).json({ message: err.message });
+}
+
+
+// app.use(function(err, req, res, next) {
+//     // set locals, only providing error in development
+//     res.locals.message = err.message;
+//     res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+//     // render the error page
+//     res.status(err.status || 500);
+//     res.render('error');
+//   });
---
+++ server.js
@@ -0,0 +1,34 @@
+﻿require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('_helpers/jwt');
+const errorHandler = require('_helpers/error-handler');
+var path = require('path');
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Enable Cross Origin Request to make front-end call our apis.
+app.use(cors());
+
+// use JWT auth to secure the api
+app.use(jwt());
+
+// api routes
+app.use('/users', require('./users/users.controller'));
+
+// global error handler
+app.use(errorHandler);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 3000;
+const server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
